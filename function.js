9a4312fe48be0eff6c1d6caecb7109dfd4ab31a8
pthanomsing <p.thanomsing@outlook.com>
2022-07-05 08:46:13 +0700

    Create JavaScript library

---
+++ function.js
@@ -0,0 +1,8 @@
+
+/* Get random number between range (min) and (max)*/
+
+   function random(min, max) {
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+}
